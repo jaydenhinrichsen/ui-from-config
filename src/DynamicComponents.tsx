47c12c7c6664154c8cfc072bfecafdfb3d215a17
Jayden Hinrichsen <jaydenhinrichsen@gmail.com>
2020-07-04 13:03:21 -0600

    Fixed types, removed use of 'anys'

--- src/DynamicComponents.tsx
+++ src/DynamicComponents.tsx
@@ -1,13 +1,31 @@
 import React, { lazy, Suspense } from 'react'
 
+interface IConfigComponent {
+  id: string;
+  component: string;
+  props: {
+    template: string;
+    [x: string]: string;
+  },
+  children: IConfigComponent[]
+}
+
+interface IDynamicComponentsProps {
+  config: IConfigComponent[];
+}
+
 // Paths to components come from frontend
 const COMPONENT_PATHS: any = {
   'Person': './Person',
 }
 
-const importedComponents = (config: any) => {
-  const components: any = {}
-  const load = (_config: any, _components: any) => {
+interface IDynamicComponents {
+  [x: string]: React.LazyExoticComponent<React.ComponentType<any>>;
+}
+
+const importedComponents = (config: IConfigComponent[]) => {
+  const components: IDynamicComponents = {}
+  const load = (_config: IConfigComponent[], _components: IDynamicComponents) => {
     for(let i = 0; i < _config.length; i++) {
       _components[_config[i].component] = lazy(() => import(`${COMPONENT_PATHS[_config[i].component]}`))
       if (_config[i].children) {
@@ -20,20 +38,19 @@ const importedComponents = (config: any) => {
   return load(config, components);
 }
 
-
-const DynamicComponents = (props: any) => {
+const DynamicComponents = (props: IDynamicComponentsProps) => {
   const config = props.config;
-  const Components = importedComponents(config)
-  const render = (c: any, i: any, _config: any) => {
+  const Components: IDynamicComponents = importedComponents(config)
+  const render = (c: IConfigComponent, i: number, _config: IConfigComponent[]) => {
     const Component = Components[c.component]
     if (_config[i].children) {
-      const children = _config[i].children.map((child: any, j: any) => render(child, j, _config[i].children));
+      const children = _config[i].children.map((child, j) => render(child, j, _config[i].children));
       return <Suspense key={c.id} fallback={<div>Loading</div>}><Component key={c.id} {...c.props}>{children}</Component></Suspense>
     } else {
       return <Suspense key={c.id} fallback={<div>Loading</div>}><Component key={c.id} {...c.props} /></Suspense>
     }
   }
-  const components = config.map((c: any, i: any) => render(c, i, config))
+  const components = config.map((c, i,) => render(c, i, config))
   return <div>{components}</div>
 }
 
